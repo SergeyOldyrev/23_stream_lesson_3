b0b75bacbb6e424bd82eeebaaa19c3de9c15e663
Sergey Oldyrev <toryn_h.e@mail.ru>
2023-10-29 10:46:25 +0300

    deleted empty strings

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -20,14 +20,12 @@ public class SimpleTest {
         $("#firstName").setValue("firstName");
         $("#lastName").setValue("lastName");
         $("#userEmail").setValue("userEmail@mail.com");
-
         $("label[for='gender-radio-1']").click();
         $("#userNumber").setValue("2222222222");
         $("#subjectsInput").setValue("subjectsInput");
         $("label[for='hobbies-checkbox-1']").click();
         $("label[for='hobbies-checkbox-2']").click();
         $("label[for='hobbies-checkbox-3']").click();
-
         $("#uploadPicture").uploadFromClasspath("test.png");
         $("#currentAddress").setValue("currentAddress");
         $("#subjectsInput").setValue("subjectsInput");
@@ -35,7 +33,5 @@ public class SimpleTest {
         $(byText("NCR")).click();
         $(byText("Select City")).click();
         $(byText("Gurgaon")).click();
-        //just for test
-
     }
 }
