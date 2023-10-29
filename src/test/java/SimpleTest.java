16d62ebef7dcac327d4ac106c784e32f4e9c83fc
Sergey Oldyrev <toryn_h.e@mail.ru>
2023-10-29 18:51:09 +0300

    Refactor + add verification in different block

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,13 +1,11 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byValue;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleTest {
 
@@ -15,7 +13,7 @@ public class SimpleTest {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen =  true;
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
@@ -30,9 +28,12 @@ public class SimpleTest {
         String city = "Gurgaon";
 
         open("/automation-practice-form");
-        $("#firstName").setValue(firstName).shouldHave(value(firstName));
-        $("#lastName").setValue(lastName).shouldHave(value(lastName));
-        $("#userEmail").setValue(userEmail).shouldHave(value(userEmail));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
         $("label[for='gender-radio-1']").click();
         $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
@@ -41,20 +42,30 @@ public class SimpleTest {
         $(".react-datepicker__year-select").click();
         $(byValue("1985")).click();
         $(".react-datepicker__day--003").click();
-        $("#dateOfBirthInput").shouldHave(value("03 Dec 1985"));
         $("#subjectsInput").setValue(subj);
         $(byText("English")).click();
-        $(".css-12jo7m5.subjects-auto-complete__multi-value__label").shouldHave(text("English"));
         $("label[for='hobbies-checkbox-1']").click();
         $("label[for='hobbies-checkbox-2']").click();
         $("label[for='hobbies-checkbox-3']").click();
         $("#uploadPicture").uploadFromClasspath("test.png");
-        $("#currentAddress").setValue(currentAddress).shouldHave(value(currentAddress));
+        $("#currentAddress").setValue(currentAddress);
         $(byText("Select State")).click();
         $(byText(state)).click();
-        $(byText(state)).shouldHave(text(state));
         $(byText("Select City")).click();
         $(byText(city)).click();
-        $(byText(city)).shouldHave(text(city));
+        $("#submit").click();
+
+        //проверяем правильность заполнения формы
+        $(byText(firstName+" "+lastName)).shouldBe(visible);
+        $(byText(userEmail)).shouldBe(visible);
+        $(byText("Male")).shouldBe(visible);
+        $(byText(userNumber)).shouldBe(visible);
+        $(byText("03 December,1985")).shouldBe(visible);
+        $(byText("English")).shouldBe(visible);
+        $(byText("Sports, Reading, Music")).shouldBe(visible);
+        $(byText("test.png")).shouldBe(visible);
+        $(byText("currentAddress")).shouldBe(visible);
+        $(byText(state+" "+city)).shouldBe(visible);
+
     }
 }
