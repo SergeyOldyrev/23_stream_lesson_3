2e193d04abdacd83878976032f6527815a6e0f80
Sergey Oldyrev <toryn_h.e@mail.ru>
2023-10-29 13:52:05 +0300

    Add verification

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,8 +1,11 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byValue;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,26 +15,46 @@ public class SimpleTest {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen =  true;
     }
 
     @Test
-    void successfulSearchTest() {
+    void successfulSearchTest() throws InterruptedException {
+        String firstName = "firstName";
+        String lastName = "lastName";
+        String userEmail = "userEmail@mail.com";
+        String userNumber = "2222222222";
+        String subj = "english";
+        String currentAddress = "currentAddress";
+        String state = "NCR";
+        String city = "Gurgaon";
+
         open("/automation-practice-form");
-        $("#firstName").setValue("firstName");
-        $("#lastName").setValue("lastName");
-        $("#userEmail").setValue("userEmail@mail.com");
+        $("#firstName").setValue(firstName).shouldHave(value(firstName));
+        $("#lastName").setValue(lastName).shouldHave(value(lastName));
+        $("#userEmail").setValue(userEmail).shouldHave(value(userEmail));
         $("label[for='gender-radio-1']").click();
-        $("#userNumber").setValue("2222222222");
-        $("#subjectsInput").setValue("subjectsInput");
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(byValue("11")).click();
+        $(".react-datepicker__year-select").click();
+        $(byValue("1985")).click();
+        $(".react-datepicker__day--003").click();
+        $("#dateOfBirthInput").shouldHave(value("03 Dec 1985"));
+        $("#subjectsInput").setValue(subj);
+        $(byText("English")).click();
+        $(".css-12jo7m5.subjects-auto-complete__multi-value__label").shouldHave(text("English"));
         $("label[for='hobbies-checkbox-1']").click();
         $("label[for='hobbies-checkbox-2']").click();
         $("label[for='hobbies-checkbox-3']").click();
         $("#uploadPicture").uploadFromClasspath("test.png");
-        $("#currentAddress").setValue("currentAddress");
-        $("#subjectsInput").setValue("subjectsInput");
-        $(".css-tlfecz-indicatorContainer").click();
-        $(byText("NCR")).click();
+        $("#currentAddress").setValue(currentAddress).shouldHave(value(currentAddress));
+        $(byText("Select State")).click();
+        $(byText(state)).click();
+        $(byText(state)).shouldHave(text(state));
         $(byText("Select City")).click();
-        $(byText("Gurgaon")).click();
+        $(byText(city)).click();
+        $(byText(city)).shouldHave(text(city));
     }
 }
